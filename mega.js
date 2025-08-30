a9e6d0fc15522db6d93b9b1166d471a173832e83
KUSAL S D <156213745+KusalSasmitba@users.noreply.github.com>
2025-08-30 23:35:01 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,7 +2,7 @@ const mega = require("megajs");
 const { Readable } = require("stream");
 
 const auth = {
-  email: "irene66226@mailbali.com",
+  email: "kaori24993@mailbali.com",
   password: "Kusalsasmitha123",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
@@ -56,3 +56,4 @@ const upload = (data, name) => {
 };
 
 module.exports = { upload };
+
